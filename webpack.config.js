157074d3fa2474e34442e0f772e681ab90323d94
Paweł Kowalski <pavelloz@gmail.com>
2016-01-12 03:44:25 +0100

    Update major version packages

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,14 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader"},
+            { 
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
             { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
             { test: /\.scss$/, loader: ExtractTextPlugin.extract('css!sass') }
         ]
